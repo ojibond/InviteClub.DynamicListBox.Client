fcaf5a2271b75be13a3f720c7333f400ba738206
ojibond <ojibond150@gmail.com>
2026-01-14 21:27:39 -0600

    EF migration setup + DB created locally

---
+++ InviteClub.DynamicListBox.Server/Migrations/20260115032627_InitialListBoxItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvitedClub.DynamicListBox.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialListBoxItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_SortOrder",
+                table: "Items",
+                column: "SortOrder");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
