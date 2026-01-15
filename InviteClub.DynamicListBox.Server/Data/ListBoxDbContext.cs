bde2b6282ccb2f10b8f2b8dfeecbbcc96e344264
ojibond <ojibond150@gmail.com>
2026-01-14 21:22:23 -0600

    Create the EF entity + DbContext (Server)

---
+++ InviteClub.DynamicListBox.Server/Data/ListBoxDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvitedClub.DynamicListBox.Server.Data;
+
+public class ListBoxDbContext : DbContext
+{
+    public ListBoxDbContext(DbContextOptions<ListBoxDbContext> options) : base(options) { }
+
+    public DbSet<ListBoxItemEntity> Items => Set<ListBoxItemEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ListBoxItemEntity>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Text).IsRequired().HasMaxLength(200);
+            e.Property(x => x.SortOrder).IsRequired();
+            e.HasIndex(x => x.SortOrder);
+        });
+    }
+}
---
+++ InviteClub.DynamicListBox.Server/Data/ListBoxItemEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace InvitedClub.DynamicListBox.Server.Data;
+
+public class ListBoxItemEntity
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public int SortOrder { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+}
